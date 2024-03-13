93c0a5a362e33af6fbe44cf01512e67587c71bfa
Anish Malla <anish.malla@selu.edu>
2024-03-13 18:49:40 -0500

    Description updated

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -116,7 +116,7 @@ const Team = () => {
       </Row>
       <TeamCard
         name="Anish Malla"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        description="A technology enthusiast pursuing a Master's in Computer Science, I'm driven by a passion for exploring cutting-edge advancements in AI, machine learning, and software development. With a strong foundation in coding and data analysis, coupled with a curiosity for innovation, I'm committed to making impactful contributions to the tech industry"
         image={Anish}
       />
     </>
